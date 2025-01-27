1a280b1d033c01f6c62e1a63835ad2c1d7f9f02a
MonikaKuchniewska <monikkuchn@gmail.com>
2025-01-27 11:50:57 +0100

    Program done

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def main():
 def change_to_string(text):
      words = text.split()
      count = len(words)
-     print (count)
+     return count 
 
-def count_characters(text):
+def count_characters(text, count):
     lowered_text = text.lower()
     count_all={}  
     for character in lowered_text:
@@ -16,12 +16,35 @@ def count_characters(text):
             count_all[character] += 1
         else:
             count_all[character] = 1
-    print (count_all)
-        
     
-  
+    # Create the report list    
+    report_list = []
+    for key in count_all:
+        report_dic = {"name": key, "num": count_all[key]}
+        report_list.append(report_dic)
+
+    # Filter only alphabetic characters
+    alp_report_list = []
+    for char_entry in report_list:
+        if char_entry["name"].isalpha():
+            alp_report_list.append(char_entry)
+   
+    # Sort the list by character count in descending order
+    alp_report_list.sort(key=lambda x: x["num"], reverse = True)
+    
+    # Print the report
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document")
+    print()
+
+    for entry in alp_report_list:
+        print(f"The '{entry['name']}' character was found {entry['num']} times")
+    
+    print("--- End report ---")
+    
+ 
 text = main()
-change_to_string(text)
-count_characters(text)
+count = change_to_string(text)
+count_characters(text, count)
 
 
